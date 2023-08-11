67ec3e7882bef6c2ecd98f0a029e1429b9199d84
mobile724.yang <mobile724.yang@foxmail.com>
2023-08-11 21:47:44 +0800

    correct_push_method

--- push.sh
+++ push.sh
@@ -1,8 +1,12 @@
-git add .
-git commit -m "$1"
-git push
+# submodule first
 hugo
 cd public
+git add .
+git commit -m "$1"
+git push origin master
+
+# then source
+cd ..
 git add .
 git commit -m "$1"
 git push origin master
